68f45d87e105d263c74921d39e978ae8172d9685
fangcan <403462630@qq.com>
2020-03-31 15:30:11 +0800

    升级gradle to 3.6.1

--- plugin/src/main/groovy/fc/plugin/multi/channel/MultiChannelPlugin.groovy
+++ plugin/src/main/groovy/fc/plugin/multi/channel/MultiChannelPlugin.groovy
@@ -3,6 +3,8 @@ package fc.plugin.multi.channel
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+import java.lang.reflect.Field
+
 /**
  * Created by rjhy on 17-1-22.
  */
@@ -18,18 +20,33 @@ class MultiChannelPlugin implements Plugin<Project> {
             project.android.applicationVariants.all { variant ->
                 def variantName = variant.name.capitalize()
                 ChannelMaker channelMaker = project.tasks.create("assemble${variantName}MultiChannel", ChannelMaker)
-//                File apkFile = variant.outputs[0].outputFile
-//                println("========" + variant.outputs[0].class + "========")
-//                println("========" + variant.getPackageApplicationProvider().get().class + "========")
-//                println("========" + apkFile.absolutePath + "========")
-//                println("========" + variant.getPackageApplicationProvider().get().outputDirectory.absolutePath + File.separator + variant.getPackageApplicationProvider().get().getApkNames()[0] + "========")
-                channelMaker.apkFile = new File(variant.getPackageApplicationProvider().get().outputDirectory.absolutePath + File.separator + variant.getPackageApplicationProvider().get().getApkNames()[0])
+//                println("====print: " + variant.outputs[0].getDirName() + ", " + variant.outputs[0].getBaseName())
+//                printProperties(variant.outputs[0])
+                def apkDir = variant.getPackageApplicationProvider().get().getOutputDirectory().getAsFile().get()
+                def apkName = variant.outputs[0].outputFileName
+//                println("文件夹: $apkDir")
+//                println("文件名: $apkName")
+//                printProperties(variant.getPackageApplicationProvider().get())
+                channelMaker.apkFile = new File(apkDir, apkName)
+//                println("*********" + channelMaker.apkFile.absolutePath)
                 channelMaker.setup()
                 channelMaker.dependsOn "assemble${variantName}"
             }
 
             ChannelMaker channelMaker = project.tasks.create("packageMultiChannel", ChannelMaker)
             channelMaker.setup()
+        }
+    }
+
+    private void printProperties(Object object) {
+        println("====printProperties: " + object.class)
+        Field[] fields = object.getClass().getFields()
+        fields.each { field ->
+            println(field.getName() + ", " + field.type)
+        }
+        println("====printMethods: " + object.class)
+        object.getClass().getMethods().each { method ->
+            println(method.getName() + ", " + method.getReturnType())
         }
     }
 }
